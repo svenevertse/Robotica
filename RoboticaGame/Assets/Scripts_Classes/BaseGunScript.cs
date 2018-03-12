9fd7b35ba38c229190106b65d3a07fc6961590d1
Sven <svenevertse@gmail.com>
2018-03-12 13:23:17 +0100

    Started with Gun classes

---
+++ RoboticaGame/Assets/Scripts_Classes/BaseGunScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseGunScript : MonoBehaviour {
+
+    public delegate void FireGun(int ammoMagazine, float fireRate);
+    public delegate void Reload(int newAmmo, float reloadSpeed);
+    public FireGun fireGun;
+    public Reload reload;
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
---
+++ RoboticaGame/Assets/Scripts_Classes/FullAutoGun.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullAutoGun : MonoBehaviour {
+
+    public BaseGunScript baseGun;
+
+    public int ammoMagazine;
+    public int magazineSize;
+
+    public float fireRate;
+    public float reloadSpeed;
+    
+
+	void Start () {
+
+        ammoMagazine = magazineSize;
+
+        baseGun.fireGun = FireGun;
+        baseGun.reload = Reload;
+
+    }
+	
+	void Update () {
+
+        baseGun.fireGun(ammoMagazine, fireRate);
+        baseGun.reload(magazineSize, reloadSpeed);
+
+    }
+
+    void FireGun (int ammo, float fireRate)
+    {
+
+        if(Input.GetButton ("Fire1") && ammoMagazine >= 1)
+        {
+
+            ammoMagazine --;
+            print(ammo);
+            print(fireRate);
+
+        }
+    }
+
+    void Reload (int newAmmo, float reloadSpeed)
+    {
+
+        if(Input.GetButtonDown("Reload"))
+        {
+
+            StartCoroutine(ReloadTimer(reloadSpeed, newAmmo));
+
+        }
+
+    }
+
+    IEnumerator ReloadTimer (float speed, int newAmmo)
+    {
+
+        yield return new WaitForSeconds(speed);
+
+        ammoMagazine = newAmmo;
+
+    }
+}
