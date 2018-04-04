b0acd1be7cc8b45fd404c5be50cea908f003bbff
Sven <svenevertse@gmail.com>
2018-04-04 16:01:18 +0200

    Main Menu Added

---
+++ RoboticaGame/Assets/Scripts_Classes/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void StartGame ()
+    {
+
+        SceneManager.LoadScene("SvenTest", LoadSceneMode.Single);
+
+    }
+
+    public void QuitGame ()
+    {
+
+        Application.Quit();
+
+    }
+}
