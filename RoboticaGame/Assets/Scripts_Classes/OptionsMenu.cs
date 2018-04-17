8b878c3463b937e6dec7d61edb8e34f6f2006a7e
Sven <svenevertse@gmail.com>
2018-04-17 12:57:01 +0200

    Main Menu Done

--- RoboticaGame/Assets/Scripts_Classes/OptionsMenu.cs
+++ RoboticaGame/Assets/Scripts_Classes/OptionsMenu.cs
@@ -1,14 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour {
 
+    public AudioMixer masterAudio;
+
+    public Dropdown resDropdown;
+    public Dropdown graphDropDown;
+
+    Resolution[] resolutions;
+
+    void Start ()
+    {
+
+        GetResolutions();
+        GetCurrentGraphics();
+
+    }
 
 	public void VolumeSetting (float volume)
     {
 
-        print(volume);
+        masterAudio.SetFloat("volume", volume);
+
+    }
+
+    public void GraphicsSettings (int qualityIndex)
+    {
+
+        QualitySettings.SetQualityLevel(qualityIndex);
+
+    } 
+
+    public void SetResolution (int resIndex)
+    {
+
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+    }
+
+    void GetResolutions ()
+    {
+
+        resolutions = Screen.resolutions;
+
+        resDropdown.ClearOptions();
+
+        List<string> resOptions = new List<string>();
+
+        int currentResolution = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+
+            string option = resolutions[i].width + "*" + resolutions[i].height;
+            resOptions.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+
+                currentResolution = i;
+
+            }
+
+        }
+
+        resDropdown.AddOptions(resOptions);
+        resDropdown.value = currentResolution;
+        resDropdown.RefreshShownValue();
+
+    }
+
+    void GetCurrentGraphics ()
+    {
+       graphDropDown.value =  QualitySettings.GetQualityLevel();
 
     }
 }
