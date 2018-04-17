b924f136b8e16439eb9f51a7c8d0591cef7755de
Sven <svenevertse@gmail.com>
2018-04-17 11:38:14 +0200

    Started Optionsmenu

---
+++ RoboticaGame/Assets/Scripts_Classes/OptionsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour {
+
+
+	public void VolumeSetting (float volume)
+    {
+
+        print(volume);
+
+    }
+}
