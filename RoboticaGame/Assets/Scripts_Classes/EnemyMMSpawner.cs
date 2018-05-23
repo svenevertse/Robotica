4245849650f28739afabe9487696b4a5a7360fc0
Sven <svenevertse@gmail.com>
2018-05-23 16:38:54 +0200

    Main menu toevoegingen

---
+++ RoboticaGame/Assets/Scripts_Classes/EnemyMMSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMMSpawner : MonoBehaviour {
+
+    public Transform[] spawnPos;
+
+    public float spawnTime;
+
+	void Start ()
+    {
+
+        StartCoroutine(Spawn(spawnTime));
+		
+	}
+
+    void SpawnIn ()
+    {
+        int randomSpawn = Random.Range(0, spawnPos.Length);
+
+        GameObject spawned = Instantiate(Resources.Load(("Sci-Fi_Soldier_MainMenu"), typeof(GameObject)), spawnPos[randomSpawn].position, Quaternion.identity) as GameObject;
+        spawned.GetComponent<EnemyMainMenu>().destinations = spawnPos;
+
+        StartCoroutine(Spawn(spawnTime));
+
+    }
+
+    IEnumerator Spawn (float spawnTime)
+    {
+
+        yield return new WaitForSeconds(spawnTime);
+
+        SpawnIn();
+
+    }
+
+}
---
+++ RoboticaGame/Assets/Scripts_Classes/EnemyMainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMainMenu : MonoBehaviour {
+
+    public NavMeshAgent agent;
+
+    public Transform[] destinations;
+
+    Vector3 distance;
+
+    public float maxDistance;
+    public Animator animator;
+
+    int randomDes;
+    float randomSpeed;
+
+    void Start () {
+
+        animator = GetComponent<Animator>();
+
+        randomDes = Random.Range(0, destinations.Length);
+        randomSpeed = Random.Range(0.6f, 0.7f);
+
+        if(randomSpeed > 0.66f)
+        {
+
+            agent.speed = 7f;
+
+        }
+        else
+        {
+
+            agent.speed = 3f;
+
+        }
+
+        agent.SetDestination(destinations[randomDes].position);
+        animator.SetFloat("Speed", randomSpeed);
+		
+	}
+	
+	void Update () {
+
+        distance.z = Vector3.Distance(transform.position, destinations[randomDes].position);
+
+        if(distance.z < maxDistance)
+        {
+
+            Destroy(gameObject);
+
+        }
+		
+	}
+}
